0753cfab272f9c70b258531fd15ae6360aa39b99
Laurent Fasnacht <fasnacht@protonmail.ch>
2021-12-21 15:35:00 +0100

    ...

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+from proton.constants import VERSION
+
+setup(
+    name="proton-core",
+    version=VERSION,
+    description="Proton Technologies API wrapper",
+    author="Proton Technologies",
+    author_email="contact@protonmail.com",
+    url="https://github.com/ProtonMail/python-proton-core",
+    install_requires=["requests", "bcrypt", "python-gnupg", "pyopenssl"],
+    packages=find_namespace_packages(include=['proton.*']),
+    include_package_data=True,
+    license="GPLv3",
+    platforms="OS Independent",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python",
+        "Topic :: Security",
+    ]
+)
