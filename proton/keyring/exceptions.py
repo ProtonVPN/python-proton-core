d89a989c6ba6fffbcf3bea932051b7f70dbdc4fe
Alexandru Cheltuitor <alexandru.cheltuitor@proton.ch>
2023-04-19 11:57:29 +0100

    Add license to individual files

--- proton/keyring/exceptions.py
+++ proton/keyring/exceptions.py
@@ -1,3 +1,23 @@
+"""
+Copyright (c) 2023 Proton AG
+
+This file is part of Proton.
+
+Proton is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Proton is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with ProtonVPN.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+
 class KeyringError(Exception):
     """Base class for Proton API specific exceptions"""
     def __init__(self, message, additional_context=None):
